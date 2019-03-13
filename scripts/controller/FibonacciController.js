5117e019e2b9410d1ced9ed0ea52cef16e5b8816
Hugo Cardoso <hjc.cad@gmail.com>
2019-03-13 12:59:12 -0300

    Calcula sequencia

--- scripts/controller/FibonacciController.js
+++ scripts/controller/FibonacciController.js
@@ -1,5 +1,26 @@
 class FibonacciController{
 
-    
+    constructor(){
+        this._sequencia = [];
+
+    }
+
+    initialize(){
+        this.calculaSequencia(3);
+    }
+
+    calculaSequencia(n){
+        var a = 0, b = 1;
+        for (var i = 2; i <= n; i++) {
+            var f = a;
+            a = b;
+            b += f;
+            console.log(b);
+        }
+        return b;
+        
+    }
+
+
 
 }
